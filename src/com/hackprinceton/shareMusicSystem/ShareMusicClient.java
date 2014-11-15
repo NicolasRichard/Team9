2c499fa47319a7727b152ca62e4c722d2a919cea
Nicolas Richard <nicrichard94@gmail.com>
2014-11-15 11:42:28 -0500

    Add classes for host receiver

--- src/com/hackprinceton/shareMusicSystem/ShareMusicClient.java
+++ src/com/hackprinceton/shareMusicSystem/ShareMusicClient.java
@@ -1,5 +1,5 @@
 package com.hackprinceton.shareMusicSystem;
 
 public final class ShareMusicClient {
-
+	
 }
--- src/com/hackprinceton/shareMusicSystem/ShareMusicHost.java
+++ src/com/hackprinceton/shareMusicSystem/ShareMusicHost.java
@@ -1,5 +1,42 @@
 package com.hackprinceton.shareMusicSystem;
 
-public final class ShareMusicHost {
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
 
+
+public final class ShareMusicHost extends BroadcastReceiver {
+    private Activity activity;
+	private Channel channel;
+	private String filesDirectory;	
+	private WifiP2pManager manager;
+    private PeerListListener peerListener;
+	
+	public ShareMusicHost(Activity activity, String filesDirectory) {
+		this.activity = activity;
+		this.filesDirectory = filesDirectory;
+		manager = (WifiP2pManager) activity.getSystemService(Context.WIFI_P2P_SERVICE);
+		channel = manager.initialize(activity, activity.getMainLooper(), null);	
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		String action = intent.getAction();
+		if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+		    // request available peers from the wifi p2p manager. This is an
+		    // asynchronous call and the calling activity is notified with a
+		    // callback on PeerListListener.onPeersAvailable()
+		    if (manager != null) {
+		        manager.requestPeers(channel, peerListener);
+		    }
+		}		
+	}	
+	
+	public void addPeerListChangedListener(PeerListListener listener) {
+		peerListener = listener;
+	}
 }
