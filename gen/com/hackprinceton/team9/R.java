caa008c83ee62abeedc8139fc60e3b96c76ffaf0
rahulamlekar <rahul.amlekar@mail.mcgill.ca>
2014-11-16 01:38:17 -0500

    commit

--- gen/com/hackprinceton/team9/R.java
+++ gen/com/hackprinceton/team9/R.java
@@ -26,8 +26,10 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int background=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int activity_main=0x7f020000;
+        public static final int background=0x7f020001;
+        public static final int buttonjoincojoy=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
         public static final int action_settings=0x7f080002;
@@ -35,10 +37,11 @@ public final class R {
         public static final int btn_start=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_hosting_start=0x7f030000;
-        public static final int activity_join=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_music_player=0x7f030003;
+        public static final int activity_buttonjoincojoy=0x7f030000;
+        public static final int activity_hosting_start=0x7f030001;
+        public static final int activity_join=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_music_player=0x7f030004;
     }
     public static final class menu {
         public static final int hosting_start=0x7f070000;
