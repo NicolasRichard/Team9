7ddf5ab94f946dc9a4a2edb4f8b442c07da06050
rahulamlekar <rahul.amlekar@mail.mcgill.ca>
2014-11-16 01:07:26 -0500

    Background added
    :dancer:

--- gen/com/hackprinceton/team9/R.java
+++ gen/com/hackprinceton/team9/R.java
@@ -13,6 +13,11 @@ public final class R {
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
@@ -21,7 +26,8 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080002;
@@ -30,13 +36,15 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_hosting_start=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_music_player=0x7f030002;
+        public static final int activity_join=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_music_player=0x7f030003;
     }
     public static final class menu {
         public static final int hosting_start=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int music_player=0x7f070002;
+        public static final int join=0x7f070001;
+        public static final int main=0x7f070002;
+        public static final int music_player=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -45,6 +53,7 @@ public final class R {
         public static final int btn_start_cojoy=0x7f050003;
         public static final int hello_world=0x7f050002;
         public static final int homepage=0x7f050005;
+        public static final int title_activity_join=0x7f050007;
         public static final int title_activity_music_player=0x7f050006;
     }
     public static final class style {
--- src/com/hackprinceton/team9/MainActivity.java
+++ src/com/hackprinceton/team9/MainActivity.java
@@ -25,7 +25,10 @@ public class MainActivity extends Activity {
 	public void startCojoy(View v) {
 		startActivity(new Intent(this, MusicPlayerActivity.class));
 	}
-
+	public void joinCojoy(View v) {
+		startActivity(new Intent(this, Join.class));
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
